b14f526925f521b562638a854e7f7e75d4896ec3
root <1057870953@qq.com>
2018-12-26 20:08:10 +0800

    11

---
+++ src/com/itheima/firstClass.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+    }
+}
